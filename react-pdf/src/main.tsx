80e7c81c733bda7ea1f778577deaa3f2168f9044
Mackgame4 <42896437+Mackgame4@users.noreply.github.com>
2024-02-17 22:20:44 +0000

    Revert "fix(pdf): Altered react to vue"
    This reverts commit d4df1a317a787c10cd38b56d5e4270d080297d4d.

---
+++ react-pdf/src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { PDFViewer } from '@react-pdf/renderer'
+import Document from './Document.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+    <PDFViewer width="100%" height="100%" style={{ border: 'none', flex: 1 }}>
+      <Document />
+    </PDFViewer>
+    </div>
+  </React.StrictMode>,
+)
